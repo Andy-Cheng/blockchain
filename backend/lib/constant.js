25db864841724cf7688af2a8c0011b8ff2bf6545
Andy-Cheng <andytony56791@gmail.com>
2018-11-21 12:16:50 +0800

    modify .gitignore

--- backend/lib/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.maxNonce = void 0;
-const maxNonce = Number.MAX_SAFE_INTEGER;
-exports.maxNonce = maxNonce;
